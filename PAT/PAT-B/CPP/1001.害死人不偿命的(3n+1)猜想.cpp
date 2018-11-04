a1458192c2fb79eb275c05f3e541dfe371b8cc25
hao14293 <hao14293@gmail.com>
2018-11-04 22:11:50 +0800

    Create 1001.害死人不偿命的(3n+1)猜想.cpp

---
+++ PAT/PAT-B/CPP/1001.害死人不偿命的(3n+1)猜想.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+  int n, count = 0;
+  cin>>n;
+  while(n!=1){
+    if(n%2!=0) n=3*n+1;
+    n/=2;
+    count++;
+  }
+  cout<<count;
+  return 0;
+}
+  
