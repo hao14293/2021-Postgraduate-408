16f5cf08813858c4ef9ed203a5fde1fce606b3f7
hao14293 <hao14293@gmail.com>
2018-11-06 20:56:43 +0800

    Create 1009.说反话.cpp

---
+++ PAT/PAT-B/CPP/1009.说反话.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+int main(){
+	stack<string> v;
+	string s;
+	while(cin >> s) v.push(s);
+	cout << v.top();
+	v.pop();
+	while(!v.empty()){
+		cout << " " << v.top();
+		v.pop();
+	}
+	return 0;
+}
