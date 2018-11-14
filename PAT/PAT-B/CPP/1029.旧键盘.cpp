d3ceea3b3d1b8b8f00e5db673b614720bfd16157
hao14293 <hao14293@gmail.com>
2018-11-14 17:49:52 +0800

    Update 1029.旧键盘.cpp

--- PAT/PAT-B/CPP/1029.旧键盘.cpp
+++ PAT/PAT-B/CPP/1029.旧键盘.cpp
@@ -4,9 +4,11 @@ using namespace std;
 
 int main(){
 	string s1, s2, ans;
+	int pos;
 	cin >> s1 >> s2;
 	for (int i = 0; i < s1.length(); i++)
 		if(s2.find(s1[i]) == string::npos && ans.find(toupper(s1[i])) == string::npos)
+//			a中包含b时 a.find(b) != string::npos
 			ans += toupper(s1[i]);
 	cout << ans;
 	return 0;
