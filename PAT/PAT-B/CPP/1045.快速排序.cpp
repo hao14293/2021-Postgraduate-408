48bc687b64efde3e4756b7b8c7b1e36cf19b0237
hao14293 <hao14293@gmail.com>
2018-11-19 17:37:43 +0800

    Update 1045.快速排序.cpp

--- PAT/PAT-B/CPP/1045.快速排序.cpp
+++ PAT/PAT-B/CPP/1045.快速排序.cpp
@@ -1,27 +1,26 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
-int a[100000], b[100000], v[100000];
+#include <algorithm>
 using namespace std;
-int main() {
-    int n, max = 0, cnt = 0;
-    scanf("%d", &n);
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &a[i]);
-        b[i] = a[i];
-    }
-    sort(a, a + n);
-    for (int i = 0; i < n; i++) {
-        if(a[i] == b[i] && b[i] > max)
-            v[cnt++] = b[i];
-        if (b[i] > max)
-            max = b[i];
-    }
-    printf("%d\n", cnt);
-    for(int i = 0; i < cnt; i++) {
-        if (i != 0) printf(" ");
-        printf("%d", v[i]);
-    }
-    printf("\n");//不加这句会有一个测试点没法通过。.
-    return 0;
+int main(){
+	int n, temp, max = 0, tmp = 0;
+	scanf("%d", &n);                  //使用cin，cout会慢很多，就这个题，用cin,cout 是114ms, 用scanf,printf是41ms.
+	vector<int> a(n), b(n), v(n);
+	for(int i = 0; i < n; i++){
+		scanf("%d",&temp);
+		b[i] = a[i] = temp;
+	}
+	sort(a.begin(), a.end());
+	for(int i = 0; i < n; i++){
+		if(a[i] == b[i] && b[i] > max)
+			v[tmp++] = b[i];
+		if(b[i] > max) max = b[i];
+	}
+	printf("%d\n", tmp);
+	for(int i = 0; i < tmp; i++){
+		printf("%d", v[i]);
+		if(i != tmp-1) printf(" ");
+	}
+	printf("\n");
+	return 0;
 }
