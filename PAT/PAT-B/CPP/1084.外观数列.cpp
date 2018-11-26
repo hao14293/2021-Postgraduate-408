2638e765fae5d315b10aa7840802e896ff3b00ad
hao14293 <hao14293@gmail.com>
2018-11-26 18:34:46 +0800

    Create 1084.外观数列.cpp

---
+++ PAT/PAT-B/CPP/1084.外观数列.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+	string s;
+	int n, j;
+	cin >> s >> n;
+	for(int cnt = 1; cnt < n; cnt++){
+		string t;
+		for(int i = 0; i < s.length(); i = j){
+			for(j = i; j < s.length() && s[j] == s[i]; j++);
+			t += to_string((s[i] - '0') * 10 + j - i);
+		}
+		s = t;
+	}
+	cout << s;
+	return 0;
+}
