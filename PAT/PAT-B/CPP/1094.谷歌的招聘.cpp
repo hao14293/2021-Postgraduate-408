2bdd527b0fe9c978dfcb1d5f6b3b10c308d9e364
hao14293 <hao14293@gmail.com>
2018-12-10 19:59:44 +0800

    Create 1094.谷歌的招聘.cpp

---
+++ PAT/PAT-B/CPP/1094.谷歌的招聘.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+bool isPrime(string s){
+	int a = stoi(s);
+	if(a < 2) return false;
+	for(int i = 2; i <= sqrt(a); i++){
+		if(a % i == 0) return false;
+	}
+	return true;
+}
+
+int main(){
+	int l, k;
+	scanf("%d%d", &l, &k);
+	string s;
+	cin >> s;
+	int flag = 0;
+	for(int i = 0; i < l - k + 1; i++){
+		string temp = s.substr(i, k);
+		if(isPrime(temp)){
+			cout << temp;
+			flag = 1;
+			break;
+		}
+	}
+	if(!flag) printf("404");
+	return 0;
+}
