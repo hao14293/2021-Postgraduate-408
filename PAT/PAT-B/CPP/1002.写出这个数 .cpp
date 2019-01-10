3c51b07ea1a9b443d4f29b15476ea0c5c4c2be51
hao14293 <hao14293@gmail.com>
2019-01-10 11:44:03 +0800

    Update 1002.写出这个数 .cpp

--- PAT/PAT-B/CPP/1002.写出这个数 .cpp
+++ PAT/PAT-B/CPP/1002.写出这个数 .cpp
@@ -1,17 +1,18 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
 int main(){
-  string s;
-  cin>>s;
-  int sum=0;
-  string str[10] = {"ling", "yi", "er", "san", "si", "wu", "liu", "qi", "ba", "jiu"};
-  for(int i=0;i<s.length();i++)
-    sum +=(s[i]-'0');
-  string num=to_string(sum);
-  for(int i=0;i<num.length();i++){
-    if(i!=0) cout<<" ";
-    cout<<str[num[i]-'0'];
-  }
-  return 0;
+	string s;
+	cin >> s;
+	int sum = 0;
+	string str[10] = {"ling", "yi", "er", "san", "si", "wu", "liu", "qi", "ba", "jiu"};
+	for(int i = 0; i < s.length(); i++)
+		sum += (s[i] - '0');
+	string sum_num = to_string(sum);
+	for(int i = 0; i < sum_num.length(); i++){
+		if(i != 0) cout << " ";
+		cout << str[sum_num[i] - '0'];
+	}
+	return 0;
 }
