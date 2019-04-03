958792b9a78e0c408755c5a36d5d2506af2cfd15
hao14293 <hao14293@gmail.com>
2019-04-03 14:11:47 +0800

    Create 1092.To Buy or Not to Buy （20 分).cpp

---
+++ PAT/PAT-A/CPP/1092.To Buy or Not to Buy （20 分).cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	string a, b;
+	cin >> a >> b;
+	for(int i = 0; i < b.length(); i++){
+		for(int j = 0; j < a.length(); j++){
+			if(a[j] == b[i]){
+				a[j] = '#';
+				b[i] = '#';
+				break;
+			}
+		}
+	}
+	int cnt1 = 0, cnt2 = 0;
+	for(int i = 0; i < b.length(); i++)
+		if(b[i] != '#') cnt1++;
+	for(int i = 0; i < a.length(); i++)
+		if(a[i] != '#') cnt2++;
+	if(cnt1 != 0) cout << "No " << cnt1;
+	else cout << "Yes " << cnt2;
+	return 0;
+}
