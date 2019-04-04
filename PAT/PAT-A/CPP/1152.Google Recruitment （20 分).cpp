e8059c202266ff33ab39e59465e9257c65609dba
hao14293 <hao14293@gmail.com>
2019-04-04 08:13:44 +0800

    Create 1152.Google Recruitment （20 分).cpp

---
+++ PAT/PAT-A/CPP/1152.Google Recruitment （20 分).cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+bool isPrime(string s){
+	int a = stoi(s);
+	if(a < 2) return false;
+	for(int i = 2; i <= sqrt(a); i++){
+		if(a % i == 0) return false;
+	}
+	return true;
+} 
+
+int main(){
+	int l, k;
+	scanf("%d %d", &l, &k);
+	string s;
+	cin >> s;
+	int flag = 0;
+	for(int i = 0; i < l - k + 1; i++){
+		string temp = s.substr(i, k);
+		if(isPrime(temp)){
+			cout << temp;
+			return 0;
+		}
+	}
+	printf("404");
+	return 0;
+}
