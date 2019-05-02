2c96c15d73bdef9b8edfc114c38d40204552cfc1
hao14293 <hao14293@gmail.com>
2019-05-02 18:43:33 +0800

    Update 1023.Have Fun with Numbers (20 分).cpp

--- PAT/PAT-A/CPP/1023.Have Fun with Numbers (20 分).cpp
+++ PAT/PAT-A/CPP/1023.Have Fun with Numbers (20 分).cpp
@@ -1,30 +1,30 @@
-#include <cstdio>
-#include <string.h>
+#include <iostream>
+#include <string>
 using namespace std;
-int book[10];
-int main() {
-    char num[22];
-    scanf("%s", num);
-    int flag = 0, len = strlen(num);
-    for(int i = len - 1; i >= 0; i--) {
-        int temp = num[i] - '0';
-        book[temp]++;
-        temp = temp * 2 + flag;
-        flag = 0;
-        if(temp >= 10) {
-            temp = temp - 10;
-            flag = 1;
-        }
-        num[i] = (temp + '0');
-        book[temp]--;
-    }
-    int flag1 = 0;
-    for(int i = 0; i < 10; i++) {
-        if(book[i] != 0)
-            flag1 = 1;
-    }
-    printf("%s", (flag == 1 || flag1 == 1) ? "No\n" : "Yes\n");
-    if(flag == 1) printf("1");
-    printf("%s", num);
-    return 0;
+
+int main(){
+	int a[10] = {0};
+	string s;
+	cin >> s;
+	int flag = 0, len = s.length();
+	for(int i = len - 1; i >= 0; i--){
+		int temp = s[i] - '0';
+		a[temp]++;
+		temp = temp * 2 + flag;
+		flag = 0;
+		if(temp >= 10){
+			temp = temp - 10;
+			flag = 1;
+		}
+		s[i] = temp + '0';
+		a[temp]--;
+	}
+	int flag1 = 0;
+	for(int i = 0; i < 10; i++){
+		if(a[i] != 0) flag1 = 1;
+	}
+	printf("%s", (flag == 1 || flag1 == 1) ? "No\n" : "Yes\n");
+	if(flag == 1) printf("1");
+	cout << s;
+	return 0;
 }
