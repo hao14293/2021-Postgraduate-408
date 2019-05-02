c38669b8767bf8ae189332fa6c4dc27c2d339e05
hao14293 <hao14293@gmail.com>
2019-05-02 17:58:50 +0800

    Update 1007.素数对猜想.cpp

--- PAT/PAT-B/CPP/1007.素数对猜想.cpp
+++ PAT/PAT-B/CPP/1007.素数对猜想.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
-bool isprime(int a){
-	for(int i=2;i*i<=a;i++)
-		if(a%i ==0) return false;
+
+bool isPrime(int n){
+	if(n < 2) return false;
+	for(int i = 2; i <= sqrt(n); i++){
+		if(n % i == 0) return false;
+	}
 	return true;
 }
+ 
 int main(){
-	int N, cnt=0;
-	cin>>N;
-	for(int i=5;i<=N;i++)
-		if(isprime(i-2) && isprime(i)) cnt++;
+	int n, cnt = 0;
+	cin >> n;
+	for(int i = 2; i <= n; i++)
+		if(isPrime(i - 2) && isPrime(i)) cnt++;
 	cout << cnt;
 	return 0;
 }
