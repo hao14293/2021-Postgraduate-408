cdc6a9db55cbce0e9647733eb65c845a0fc7d3bb
hao14293 <hao14293@gmail.com>
2019-06-25 09:27:22 +0800

    Create 1020 Tree Traversals (25 分).cpp

---
+++ PAT/PAT-A/CPP/1020 Tree Traversals (25 分).cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+vector<int> PostOrder, InOrder, Level(10000, -1);
+
+void getLevel(int root, int start, int end, int index);
+
+int main(){
+	int n, cnt = 0;
+	cin >> n;
+	PostOrder.resize(n);
+	InOrder.resize(n);
+	for(int i = 0; i < n; i++)
+		cin >> PostOrder[i];
+	for(int i = 0; i < n; i++)
+		cin >> InOrder[i];
+	getLevel(n - 1, 0, n - 1, 0);
+	for(int i = 0; i < Level.size(); i++){
+		if(Level[i] != -1 && cnt != n - 1){
+			cout << Level[i] << " ";
+			cnt++;
+		}else if(Level[i] != -1){
+			cout << Level[i];
+			break;
+		}
+	}
+	return 0;
+}
+
+void getLevel(int root, int start, int end, int index){
+	if(start > end) return ;
+	int i = start;
+	while(i < end && InOrder[i] != PostOrder[root]) i++;
+	Level[index] = PostOrder[root];
+	getLevel(root - 1 - end + i, start, i  -1, 2 * index + 1);
+	getLevel(root - 1, i + 1, end, 2 * index + 2);
+}
